4c17cdad143ac283c6dd307816b88b5d0d7880b6
Mikko Kiander <mikko@statzon.com>
2022-03-13 23:00:06 +0200

    Added command line messaging about progress

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 - Add input_new.csv and input_old.csv
 - Run script app.py (python3 app.py)
 - Script will generate file output.csv
-- Rows in input_new.csv that are not found in input_old.csv are written to output.csv
+- Rows in input_new.csv that are not found in input_old.csv are appended to output.csv
+- If output.csv does not exist, the first row of new file will be written to output.csv (header row)
--- app.py
+++ app.py
@@ -1,23 +1,39 @@
 import csv
+import sys
+import os.path
 
 new_file = "input_new.csv"
 old_file = "input_old.csv"
-output_path = "output.csv"
+output_file = "output.csv"
 
 new_file_rows = []
 old_file_rows = []
 diff_rows = []
 
+sys.stdout.write('Gathering rows from input files...\n')
+
+new_rows = 0
 with open(new_file) as fin_new:
     read_new = csv.reader(fin_new)
     for row_new in read_new:
         new_file_rows.append(row_new)
+        new_rows = new_rows + 1
+sys.stdout.write('New file has %s rows\n' % new_rows)
+
+old_rows = 0
 with open(old_file) as fin_old:
     read_old = csv.reader(fin_old)
     for row_old in read_old:
         old_file_rows.append(row_old)
+        old_rows = old_rows + 1
+sys.stdout.write('Old file has %s rows\n' % old_rows)
 
-diff_rows.append(new_file_rows[0])
+if (os.path.isfile(output_file) == False):
+    diff_rows.append(new_file_rows[0])
+
+sys.stdout.write('\nComparing rows...\n')
+rows_processed = 0
+unique = 0
 
 for row_new in new_file_rows:
     row_found = 0
@@ -26,7 +42,16 @@ for row_new in new_file_rows:
             row_found = 1
     if (row_found == 0):
         diff_rows.append(row_new)
+        unique = unique + 1
+    rows_processed = rows_processed + 1
+    percentage = rows_processed / new_rows * 100
+    sys.stdout.write(' (%d%%) %s / %s rows compared (%d non-matched)\r' % (percentage,rows_processed,new_rows, unique))
+    sys.stdout.flush()
 
-with open(output_path, 'w') as fout:
+sys.stdout.write('\n\nWriting non-matched rows to output.csv...')
+
+with open(output_file, 'a+') as fout:
     writer = csv.writer(fout)
     writer.writerows(diff_rows)
+
+sys.stdout.write('\nDone!\n')
